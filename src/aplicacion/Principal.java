9e07ceaf15d680d72dd992050867b3d6ffb6b1ef
adillo8 <pilar.fernandezadillo@usp.ceu.es>
2024-02-12 21:58:33 +0100

    "errores de paquetes y desviacion tipica"

---
+++ src/aplicacion/Principal.java
@@ -0,0 +1,33 @@
+package aplicacion;
+import mates.*;
+public class Principal {
+    public static void main(String[] args) {
+        //ejerccio 4 apartado 1
+        System.out.println("la suma de los numeros hasta el 10 es "+Funciones.sumaNaturales(10));
+        //ejercicio 4 apartado 2
+        System.out.println("el factorial de 10 es "+Funciones.factorial(10));
+        //ejercicio 4 apartado 3
+        System.out.println("la potencia n-esima de 2 es "+Funciones.potenciaNesima(2));
+        //ejercicio 4 apartado 4
+        int[] lista = {1,2,3,4,5};
+        System.out.println("la suma de los numeros de la lista es "+Funciones.sumaListaNumeros(lista));
+        //ejercicio 4 apartado 5    
+        System.out.println("la media de los numeros de la lista es "+Funciones.mediaListaNumeros(lista));
+        //ejercicio 4 apartado 6 NO SE COMO HACERLO
+        System.out.println("la desviacion tipica de los numeros de la lista es ");
+        //ejercicio 4 apartado 7
+        System.out.println("La suma de los primeros números pares hasta n es "+Funciones.sumaPares(9));
+        //ejercicio 4 apartado 8
+        System.out.println("La suma de los elementos pares de una lista de enteros es "+Funciones.sumaParesDos(lista));
+        //ejercicio 4 apartado 9
+        System.out.println("La lista con los números pares de la lista inicial es "+Funciones.obtenerListaPar(lista));
+        //ejercicio 4 apartado 10
+        System.out.println("La lista de los primeros números pares hasta n es "+Funciones.listaPares(9));
+        //ejercicio 4 apartado 11
+        int[] lista2 = {1,2,3,};
+        int[] lista3 = {2,4,6};
+        System.out.println("Producto escalar de dos listas de números es "+Funciones.productoVectorial(lista2, lista3));
+    }
+
+    
+}
---
+++ src/mates/Funciones.java
@@ -0,0 +1,122 @@
+package mates;
+public class Funciones {
+
+    //ejercicio 4 apartado 1
+    public static int sumaNaturales(int n){
+        int acumulado = 0;
+        //en el for pueden no ponerse llaves 
+        for (int i = 1; i <= n; i++) 
+            //no se pone acumulado++ porque solo se le sumaria +1 +1 
+            acumulado +=i;
+        
+        return acumulado;
+
+    }
+
+    //ejercicio 4 apartado 2
+    public static int factorial(int n){
+        int acumulado = 1;
+        for (int i = 1; i <= n; i++) 
+        //acumulado=acumulado*i    
+        acumulado *= i;
+        
+        return acumulado;
+    }
+
+    //ejercicio 4 apartado 3
+    public static int potenciaNesima(int n){
+        int acumulado = 1;
+        for (int i = 1; i <= n; i++) 
+        //acumulado=acumulado*n    
+        acumulado *= n;
+        
+        return acumulado;
+    }
+
+    //ejercicio 4 apartado 4
+    public static int sumaListaNumeros(int[] lista){
+        int acumulado = 0;
+        for (int i = 0; i < lista.length; i++) 
+        //acumulado=acumulado+lista[i]    
+        acumulado += lista[i];
+        
+        return acumulado;
+    }
+
+    //ejercicio 4 apartado 5
+    public static  int mediaListaNumeros(int[] lista){
+        int acumulado = 0;
+        for (int i = 0; i < lista.length; i++) 
+        acumulado = (acumulado+lista[i])/lista.length;
+
+        return acumulado;
+        
+        
+    }
+
+    //ejercicio 4 apartado 6
+    public static void desviacionTipica(){
+        //COMO SE HACE ESTO????
+
+        
+        
+    }
+
+    //ejercicio 4 apartado 7
+    public static int sumaPares(int n){
+        int acumulador = 0;
+        for(int i = 0; i <= n; i++){
+            if(i % 2 == 0){
+                acumulador += i;
+            }
+        }
+        return acumulador;
+       
+    }
+
+    //ejercicio 4 apartado 8
+    public static int sumaParesDos(int[] arrayList){
+        int acumulador = 0;
+        //el valor que tenga el array se pone en la i con el for each
+        for(int i : arrayList){
+            if(i%2 == 0){
+                acumulador += i;
+            }
+        }
+        return acumulador;
+    }
+
+    //ejercicio 4 apartado 9
+    public static String obtenerListaPar(int[] arrayList){
+        String listaPares = "";
+        for(int i : arrayList){
+            if(i%2 == 0){
+                listaPares += i + " ";
+            }
+        }
+        return listaPares;
+    }
+
+    //ejercicio 4 apartado 10
+    public static String listaPares(int n){
+        String lista = " ";
+        //a i se le asigna el valor de n y mientras i sea mayor o igual a 2 se le resta 1
+        for(int i = n ; i >= 2; i--){
+            if(i%2 == 0){
+                lista += i + ",";
+            }
+        }
+        return lista;
+    }
+
+    //ejercicio 4 apartado 11
+    public static int productoVectorial(int[] arrayList1,int[] arrayList2){
+        int acumulador = 0;
+        for(int i = 0; i < arrayList1.length; i++){
+            acumulador+= arrayList1[i] * arrayList2[i];
+        }
+
+        return acumulador;
+    }
+
+}
